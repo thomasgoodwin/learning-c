b3bff18611cd495407d887162bdee24b64c838a9
thomasgoodwin <thomasg@bittnerdev.com>
2025-07-14 23:44:40 -0700

    add solution

--- src/level-one/main.c
+++ src/level-one/main.c
@@ -1,28 +1,91 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include <stdio.h> // printf, scanf
+#include <stdbool.h>  // true, false
+#include <string.h> // strcmp
+#include <time.h> // time
+#include <stdlib.h> // srand, rasnnd
+#include <ctype.h> // tolower
 
-bool gameLoop()
+#define MAX_BUFFER_SIZE 512
+
+enum Choice
 {
-  printf("Welcome, please choose between Rock, Paper, or Scissors.\n If you would like to exit the game, type exit.");
+  ROCK,
+  PAPER,
+  SCISSORS,
+  CHOICE_COUNT
+};
 
-  // TODO: Check for exit and exit if necessary
+void gameLoop()
+{
+  int playerWins = 0;
+  int computerWins = 0;
+  while (true)
+  {
+    char userInput[MAX_BUFFER_SIZE];
+    printf("Please choose between Rock, Paper, or Scissors.\nIf you would like to exit the game, type exit.\n");
+    scanf("%s", userInput);
 
-  // TODO: Recieve and store user input
-  
-  // TODO: Randomly pick r/p/s for the computer 
-  
-  // TODO: Compare user and computer picks and announce winnner
+    for (int i = 0; userInput[i]; i++)
+    {
+      userInput[i] = tolower(userInput[i]);
+    }
 
-  // TODO: Track and display scoreline
+    if (strcmp(userInput, "exit") == 0)
+    {
+      break;
+    }
+    int usersChoice;
+    if (strcmp(userInput, "rock") == 0)
+    {
+      usersChoice = ROCK;
+    }
+    else if (strcmp(userInput, "paper") == 0)
+    {
+      usersChoice = PAPER;
+    }
+    else if (strcmp(userInput, "scissors") == 0)
+    {
+      usersChoice = SCISSORS;
+    }
+    else
+    {
+      printf("Input Invalid. Try again.\n");
+      continue;
+    }
+
+    int computersChoice = rand() % CHOICE_COUNT;
+
+    if (usersChoice == computersChoice)
+    {
+      printf("A Tie!\n");
+    }
+    else if (usersChoice == ROCK && computersChoice == PAPER)
+    {
+      printf("You Lost!\n");
+      computerWins++;
+    }
+    else if (usersChoice == PAPER && computersChoice == SCISSORS)
+    {
+      printf("You Lost!\n");
+      computerWins++;
+    }
+    else if (usersChoice == SCISSORS && computersChoice == ROCK)
+    {
+      printf("You Lost!\n");
+      computerWins++;
+    }
+    else
+    {
+      printf("You Won!\n");
+      playerWins++;
+    }
+    printf("You have %d wins and the computer has %d wins.\n\n", playerWins, computerWins);
+  }
 }
 
 int main(void)
 {
-  bool runGameLoop = true;
-  while (runGameLoop)
-  {
-    bool runGameLoop = gameLoop();
-  }
-
+  srand(time(NULL));
+  gameLoop();
   return 0;
 }
